7a845211a6622557b9ccae9d6a92f9100807aa5c
zotherstupidguy <zotherstupidguy@gmail.com>
2016-01-10 23:22:29 +0200

    adding ttyrecorded files on github

--- ttyplayer.rb
+++ ttyplayer.rb
@@ -1,4 +1,8 @@
 require "base64"
+
+
+#To use it in ruby
+"<tty-player autoplay loop src=\"data:application/x-ttyrec;base64, #{content}\"></tty-player>"
+
 content = File.read('ttyrecord')
-puts Base64.encode64 content
-#puts Base64.decode64 content
+puts content = Base64.encode64 content
