d8103c437f63253953d41905468555625a28848d
zotherstupidguy <zotherstupidguy@gmail.com>
2016-03-10 19:45:20 +0200

    changed ruby files from the frontend github-hosted website

--- ttyplayer.rb
+++
@@ -1,8 +0,0 @@
-require "base64"
-
-
-#To use it in ruby
-"<tty-player autoplay loop src=\"data:application/x-ttyrec;base64, #{content}\"></tty-player>"
-
-content = File.read('ttyrecord')
-puts content = Base64.encode64 content
