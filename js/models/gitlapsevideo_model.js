64d5b7cf32761099baced772e73ec0edc56187a4
zotherstupidguy <zotherstupidguy@gmail.com>
2016-04-07 10:19:35 +0200

    remove , from gitlapsevideo_model

--- js/models/gitlapsevideo_model.js
+++ js/models/gitlapsevideo_model.js
@@ -2,18 +2,17 @@
 var GitlapseVideo = Backbone.Model.extend({
   // urlRoot : '/v1/lapses',
   urlRoot : 'https://api.gitlapse.com/v1/lapses/',
-  defaults: {
-    title: 'some title',
+    defaults: {
+      title: 'some title',
     content: 'an placeholder advertisment for coder link'
-  },
+    },
     // Validate when we are setting an attribute
     validate: function(attrs){
       if ( ! attrs.title) {
 	"Every video must have a title"
-      },
-    if ( ! attrs.content ) {
-      "Every video must have a content"
-    }
+      }
+      if ( ! attrs.content ) {
+	"Every video must have a content"
+      }
     }
 });
-
